95b6fd047bb3150f7bbadfda83d290cd93bd69eb
Christophe Bonneville <cpb97@cornell.edu>
2021-04-29 15:50:27 -0400

    Modified to handle non linear element matrices

--- Assembly.m
+++ Assembly.m
@@ -13,31 +13,33 @@ numEq=MeshStruct.numEq;
 gatherMat=MeshStruct.gatherMat;
 
 % initialize the global system
-K=zeros(numEq,numEq); 
-F=zeros(numEq,1);
+Kt = zeros(numEq,numEq); 
+F = zeros(numEq,1);
+R = zeros(numEq,1);
 
 
 % get the appropriate quadrature locations and weights
 [qp,w] = Gauss(nnpe);
 for e=1:numEls
-    [ke,fe] = TwoDElem(MeshStruct, e, qp, w); % make the local stiffness matrix and 
+    [kte,fe,re] = TwoDElemNL(MeshStruct, e, qp, w, d); % make the local stiffness matrix and 
                                               % local force vector for this element
     
     for Lrw = 1 : (nnpe*numDOF)
         Grw = gatherMat(e,Lrw); % global row index
         F(Grw) = F(Grw) + fe(Lrw);
+        R(Grw) = R(Grw) + re(Lrw);
         for Lcl = 1 : (nnpe*numDOF)
             Gcl = gatherMat(e,Lcl); % global column index
             
             % Assemble local stiffness matrix into the global
             % stiffness matrix here
-            K(Grw,Gcl) = K(Grw,Gcl) + ke(Lrw,Lcl);
-            
+            Kt(Grw,Gcl) = Kt(Grw,Gcl) + kte(Lrw,Lcl);
         end
     end
 end
 
 % Package variables into the output structs. NOTE: both K and F are made
 % sparse to greatly reduce solution time.
-globalSystem.K=sparse(K);
+globalSystem.Kt=sparse(Kt);
 globalSystem.F=sparse(F);
+globalSystem.R=sparse(R);
