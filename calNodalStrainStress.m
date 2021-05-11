a46769388f19ad87f70fe123b1d4374ef49e68fe
sms722 <56262896+sms722@users.noreply.github.com>
2021-05-11 18:15:03 -0400

    Update calNodalStrainStress.m

--- calNodalStrainStress.m
+++ calNodalStrainStress.m
@@ -1,4 +1,4 @@
-function [strain, stress] = calNodalStrainStress(glU,meshStruct)
+function [strain, stress] = calNodalStrainStress(glU,meshStruct,globalSystem)
 %nodalGradient = calNodalGradient(glField,meshStruct)   Calculate nodal
 %stress and strain
 %
@@ -38,7 +38,8 @@ numEls=meshStruct.numEls;
 nCoords=meshStruct.nCoords;
 elCon=meshStruct.elCon;
 gatherMat=meshStruct.gatherMat;
-D=meshStruct.Material.D;
+d=globalSystem.d;
+ConstitutiveLaw=meshStruct.ConstitutiveLaw;
 
 % Initialization
 R = zeros(numNodes,6);  % right hand side (3 stress components + 3 strain components)
@@ -51,14 +52,22 @@ for e = 1:numEls
     Me = zeros(nnpe);        
     Re = zeros(nnpe,6);
     
-    xynode = nCoords(elCon(e,:),:); % nodal coordinate
+    % get nodal coordinates for this element
+    xvec = nCoords(elCon(e,:), 1);
+    yvec = nCoords(elCon(e,:), 2); 
+
+    % nodal x-coordinate in current configuration
+    ux = xvec + d(elCon(e, :) * 2 - 1); 
+    % nodal y-coordinate in current configuration
+    uy = yvec + d(elCon(e, :) * 2);
+    
     lcU = glU(gatherMat(e,:)); % local displacement
     for iqp = 1:length(w)
         NofXiEta=Nmatrix(qp(iqp,:),nnpe); % row vector of shape functions
                                           % at this QP in parent domain
         dNdXiEta=dNmatrix(qp(iqp,:),nnpe);% 2xnnpe array of shape func derivs
                                       % at this QP in parent domain
-        JofXiEta=dNdXiEta*xynode;    % jacobian at this QP (2x2 matrix)
+        JofXiEta = dNdXiEta * [xvec, yvec]; % jacobian at this QP 
         dNdXY=JofXiEta\dNdXiEta;     % 2xnnpe array of dNdX at ths QP
      
         % ----- Deformation gradient F_e (Slide 13/18 - Eq 24) -------- %
@@ -69,23 +78,39 @@ for e = 1:numEls
             F_e = F_e + F_e_np; 
         end
         
+        % ---------- Right Cauchy-Green strain tensor C_e ------------- %
+        C_e = F_e' * F_e;
+        
         % ---------------- Material stiffness matrix D ---------------- %
         switch ConstitutiveLaw
             case 'compressibleNeoHookean'
-                % solve for D here for the element and qp, which is  
-                % dependent on C_e, det(F_e), and the Lame constants
+                J_e = det(F_e);
+                invC_e = inv(C_e);
+                beta = meshStruct.Material.lambda;
+                mu = meshStruct.Material.mu;
+
+                D11 = (beta * J_e + 2 * mu) * invC_e(1, 1) ^ 2;
+                D22 = (beta * J_e + 2 * mu) * invC_e(2, 2) ^ 2;
+                D33 = - (beta * (J_e - 1) * J_e - mu) * invC_e(1, 1) * invC_e(2, 2) + (beta * J_e ^ 2 + mu) * invC_e(1, 2) ^ 2;
+                D12 = - 2 * (beta * (J_e - 1) * J_e - mu) * invC_e(1, 2) ^ 2 + beta * (2 * J_e - 1) * J_e * invC_e(1, 1) * invC_e(2, 2);
+                D13 = (beta * J_e + 2 * mu) * invC_e(1, 1) * invC_e(1, 2);
+                D23 = (beta * J_e + 2 * mu) * invC_e(1, 2) * invC_e(2, 2);
+
+                D = [D11, D12, D13; D12, D22, D23; D13, D23, D33];
             case 'StVenant'
                 D=meshStruct.Material.D;
         end
         
         % ------ Euler Lagrange Tensor Ee (Slide 13/18 - Eq 25) ------- %
-        E_e = 0.5 * (C_e - eye(2));
+        E_e = 0.5 * (C_e - eye(2)); % Matrix notation
+        E_e_V = [E_e(1, 1); E_e(2, 2); 2 * E_e(1, 2)]; % Voigt notation
         
         % ------------ Second PK Stress (Slide 12/18 - Eq 23) --------- %
-        S_e = D * [E_e(1, 1); E_e(2, 2); 2 * E_e(1, 2)]; % Voigt notation
+        S_e = D * E_e_V; % Voigt notation
+        S_e_M = [S_e(1), S_e(3); S_e(3), S_e(2)]; % Matrix notation
         
         Me = Me + NofXiEta'*NofXiEta * w(iqp)*det(JofXiEta);
-        Re = Re + NofXiEta'* [E_e',S_e'] * w(iqp)*det(JofXiEta);
+        Re = Re + NofXiEta'* [E_e_V',S_e'] * w(iqp)*det(JofXiEta);
 
     end
     
